be239a3f6d3d48eb6715406ae134158085452773
Chris Czub <chris.czub@gmail.com>
2012-07-16 15:11:22 -0400

    Fix MSIE7 position:absolute/float bug

--- jquery.alba.eclipse.js
+++ jquery.alba.eclipse.js
@@ -12,6 +12,12 @@ jQuery(function($) {
 				borderColor: 'transparent',
 				borderStyle: 'solid'
 			}).addClass('alba-placeholder').insertBefore(this.element);
+			// MSIE7 has a bug wherein position:absolute; elements
+			// next to float: elements aren't necessarily really
+			// "absolute" and will kind of go whereever they want.
+			// adding this empty span prevents this bug.
+			if($.browser.msie && $.browser.version < 8)
+				$('<span></span>').insertAfter(this.clone);
 			
 			// so we can see it
 			if (this.options.debug) this.clone.css({ background: 'rgba(0, 0, 255, 0.1)' });
